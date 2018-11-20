6ac069ea11d75352cf19af8896769758125f98fa
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-11-19 20:49:47 -0800

    Set up skeleton of basic components, imported Semantic UI label

---
+++ src/components/EpisodeTag.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Label } from 'semantic-ui-react'
+
+class EpisodeTag extends Component {
+//iterate through each episode to create a label with coordinates
+
+    render(){
+        return (
+            <div>
+                <Label>
+
+                </Label>
+            </div>
+        )
+    }
+}
+
+export default EpisodeTag
---
+++ src/containers/WorldMap.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { EpisodeTag } from '../components/EpisodeTag'
+
+class WorldMap extends Component {
+//import API for world map here or in App.js ?
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default WorldMap
