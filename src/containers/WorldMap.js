5a4d15bf04ccd1d53cfc973262b6460f31adf056
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-11-24 22:58:01 -0800

    Started Mapbox component

--- src/containers/WorldMap.js
+++ src/containers/WorldMap.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import { EpisodeTag } from '../components/EpisodeTag'
+import ReactMapboxGl, { Layer, Marker } from "react-mapbox-gl";
+import { EpisodeTag } from '../components/EpisodeTag';
 
 class WorldMap extends Component {
-//import API for world map here or in App.js ?
-    render(){
-        return (
-            <div>
 
-            </div>
-        )
+    state = {
+        Map: null
+    }
+
+    componentDidMount(){
+        if (process.env.MAPBOXKEY){
+            this.setState({
+                Map: ReactMapboxGl({
+                    accessToken: process.env.MAPBOXKEY
+                })
+            })
+        }
+    }
+    render(){
+    
+        const {Map} = this.state
+
+        return Map? (
+        <Map
+          style={"mapbox://styles/kristenleach/cjowhw8p5419f2srpcpfhmp5s"}
+          containerStyle={{
+            height: "300px",
+            width: "400px",
+          }}>
+        </Map>
+        ): null
     }
 }
 
