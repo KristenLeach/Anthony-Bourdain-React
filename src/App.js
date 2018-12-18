ef175696e81f9f00dfa94e62fdf34f68c6634b9a
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-12-17 22:50:26 -0800

    Added menu to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import WorldMap from './containers/WorldMap';
+import Menu from './containers/Menu';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Menu/>
         <WorldMap/>
       </div>
     );
